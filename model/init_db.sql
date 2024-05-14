8b13f64cde27c49aea026e1e9d014f72237c4b17
Shannon Palumbo <sspalumbo@gmail.com>
2024-05-14 11:43:22 +0200

    imported table

--- model/init_db.sql
+++ model/init_db.sql
@@ -3,16 +3,17 @@
 --
 
 SET foreign_key_checks = 0;
-DROP TABLE if exists students;
+DROP TABLE if exists steps;
 SET foreign_key_checks = 1;
 
 --
 -- Create Tables
 --
 
-CREATE TABLE students(
-    id INT NOT NULL AUTO_INCREMENT, 
-    firstname VARCHAR(40) not null, 
-    lastname VARCHAR(40) not null, 
-    PRIMARY KEY (id)
-    );
+CREATE TABLE `steps`(
+    `id` BIGINT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    `Description` VARCHAR(255) NOT NULL,
+    `Next_1` BIGINT NOT NULL,
+    `Next_2` BIGINT NOT NULL,
+    `Next_3` BIGINT NOT NULL
+);
