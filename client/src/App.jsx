96ffe3909991b1dae859680129c1c7efad48d476
Shannon Palumbo <sspalumbo@gmail.com>
2024-05-15 12:05:02 +0200

    adding some pages

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,34 +2,43 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import { Routes, Route, Link} from "react-router-dom"; //import Routes and Route from react router dom 
+
+import Home from "./pages/home"; // import each page up here in this format that links to file folder
+import Start from './pages/start';
+
 
 function App() {
-  const [count, setCount] = useState(0)
+  
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <h1>Testing! Is this thing on?</h1>
+      <h2>What is this thing?</h2>
+
+      <nav>
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+            <Link to="/start">Let's Get Started!</Link>
+
+          </li>
+        </ul>
+      </nav>
+
+      {/* Wrap Routes in Routes tag and add each page as a route path in return */}
+      <Routes> 
+        <Route path="/" element={<Home />}  />
+        <Route path="/start" element={<Start />}  />
+
+
+
+      </Routes>
+
+
+
     </>
-  )
+  );
 }
 
 export default App
